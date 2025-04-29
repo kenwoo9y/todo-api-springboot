3ced4d9c551ba1c09176cf8884ef9a03c20ff57c
kenwoo9y <kenwoo9y@gmail.com>
2025-04-29 17:17:08 +0900

    setup migration

---
+++ api/src/main/resources/db/migration/V1__create_users_and_tasks_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) UNIQUE,
+    email VARCHAR(80) UNIQUE,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tasks (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    description VARCHAR(255),
+    due_date DATE,
+    status VARCHAR(10),
+    owner_id BIGINT,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+); 
