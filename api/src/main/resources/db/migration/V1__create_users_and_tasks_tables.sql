4e71e4574729928106695af47659c2cdd6250556
kenwoo9y <kenwoo9y@gmail.com>
2025-05-04 12:52:08 +0900

    add postgresql ddl

--- api/src/main/resources/db/migration/V1__create_users_and_tasks_tables.sql
+++ api/src/main/resources/db/migration/V1__create_users_and_tasks_tables.sql
@@ -1,3 +1,4 @@
+-- MySQL用のDDL
 CREATE TABLE users (
     id BIGINT NOT NULL AUTO_INCREMENT,
     username VARCHAR(30) UNIQUE,
@@ -20,4 +21,31 @@ CREATE TABLE tasks (
     updated_at DATETIME(6) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users(id)
-); 
+);
+
+-- PostgreSQL用のDDL
+/*
+CREATE TABLE users (
+    id BIGSERIAL NOT NULL,
+    username VARCHAR(30) UNIQUE,
+    email VARCHAR(80) UNIQUE,
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tasks (
+    id BIGSERIAL NOT NULL,
+    title VARCHAR(30),
+    description VARCHAR(255),
+    due_date DATE,
+    status VARCHAR(10),
+    owner_id BIGINT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+*/
